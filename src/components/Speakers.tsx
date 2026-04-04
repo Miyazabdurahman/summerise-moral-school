3c00acd9f20e786bc7392290e67dd098fa669d92
Miyaz Abdurahman <miyazabdurahman@gmail.com>
2026-04-04 21:36:53 +0530

    Updatedd

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -16,7 +16,7 @@ export default function Speakers() {
       name: 'Aman',
       title: 'Motivational Speaker',
       description: 'Inspiring youth advocate and TEDx speaker on personal growth and values.',
-      image: '/aman.jpg?auto=compress&cs=tinysrgb&w=400',
+      image: '/aman.jpeg?auto=compress&cs=tinysrgb&w=400',
     },
     {
       name: 'Adil',
