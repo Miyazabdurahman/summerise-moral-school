b41f16835eabc619edf16288a81da4e2eda88e58
Miyaz Abdurahman <miyazabdurahman@gmail.com>
2026-04-06 11:40:48 +0530

    ssd

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
       <Partners />
       <CTA />
       <Footer />
+      <Analytics />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { Analytics } from "@vercel/analytics/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <Analytics />
+    </>
   </StrictMode>
 );
